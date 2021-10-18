577a30f5cce82e74e61ba556a71687b350344845
Loris Leiva <loris.leiva@gmail.com>
2021-10-18 10:28:00 +0100

    wip

--- app/src/vue-adapter/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './errors';
-export * from './useLocalStorage';
-export * from './useWallet';
-export { default as WalletProvider } from './WalletProvider';
--- app/src/vue-adapter/useLocalStorage.d.ts
+++
@@ -1,2 +0,0 @@
-import { Ref } from '@vue/reactivity';
-export declare function useLocalStorage<T>(key: string, defaultValue?: T | null): Ref<T | null>;
