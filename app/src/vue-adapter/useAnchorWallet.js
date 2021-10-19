b4cea675134c9fa223ddc712918c61ec80b0edb7
Loris Leiva <loris.leiva@gmail.com>
2021-10-19 12:04:08 +0100

    wip

--- app/src/vue-adapter/useAnchorWallet.js
+++ app/src/vue-adapter/useAnchorWallet.js
@@ -1,13 +1,13 @@
 import { computed } from '@vue/reactivity';
 import { useWallet } from './useWallet';
 export function useAnchorWallet() {
-    const wallet = useWallet();
+    const walletStore = useWallet();
     return computed(() => {
-        // Ensure the wallet was initialised by a WalletProvider.
-        if (!wallet)
+        // Ensure the wallet store was initialised by a WalletProvider.
+        if (!walletStore)
             return;
-        // Ensure the wallet is connect and supports the right methods.
-        const { signTransaction, signAllTransactions, publicKey } = wallet;
+        // Ensure the wallet is connected and supports the right methods.
+        const { signTransaction, signAllTransactions, publicKey } = walletStore;
         if (!signTransaction.value || !signAllTransactions.value || !publicKey.value)
             return;
         return {
